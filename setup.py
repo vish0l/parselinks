22e1cdc2c93f99bc060a9ac7c68c2bbf5b3f5c6b
vishal singh <join4m.com@gmail.com>
2018-11-25 19:19:25 +0530

    updated requirements

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/venins/parselinks",
-    packages=setuptools.find_packages(),
+    packages=['lxml==4.2.5', 'requests==2.20.1'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
